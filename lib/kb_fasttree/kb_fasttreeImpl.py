451854f10052be4f31ad0ca47c0f7bb92c68d023
Dylan Chivian <DCChivian@lbl.gov>
2016-04-21 15:47:20 -0700

    debugging

--- lib/kb_fasttree/kb_fasttreeImpl.py
+++ lib/kb_fasttree/kb_fasttreeImpl.py
@@ -474,8 +474,8 @@ class kb_fasttree:
         p = subprocess.Popen(fasttree_cmd, \
                              cwd = self.scratch, \
                              stdin = subprocess.PIPE, \
-                             stdout = subprocess.STDOUT, \
-                             stderr = subprocess.STDOUT, \
+                             stdout = subprocess.PIPE, \
+                             stderr = subprocess.PIPE, \
                              shell = True, \
                              env = env)
 #                             executable = '/bin/bash' )
