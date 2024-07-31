adc177e6ee74c2b28c1afc23b91010068093ac62
ialarmedalien <ialarmedalien@gmail.com>
2024-07-31 05:42:03 -0700

    updating test workflow; adding dependabot and codeQL; adding compile report; removing unneeded parts of Makefile; adding pyproject file for Ruff linting/checks

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,5 +3,14 @@ script_dir=$(dirname "$(readlink -f "$0")")
 export KB_DEPLOYMENT_CONFIG=$script_dir/../deploy.cfg
 export KB_AUTH_TOKEN=`cat /kb/module/work/token`
 export PYTHONPATH=$script_dir/../lib:$PATH:$PYTHONPATH
-cd $script_dir/../test
-python -u -m unittest discover -p "*_test.py"
+
+# run without collecting coverage data
+# pytest -vv test
+
+# collect coverage data
+pytest \
+    --cov=lib/ \
+    --cov-config=.coveragerc \
+    --cov-report=html \
+    --cov-report=xml \
+    test
